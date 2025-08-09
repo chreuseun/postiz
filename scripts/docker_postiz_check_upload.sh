e6f475f7febb436e6275c05f7f2b6cc241a9a5a9
Eunille.Villegas <eunille.villegas@growsari.com>
2025-08-09 17:58:46 +0800

    update

--- scripts/docker_postiz_check_upload.sh
+++ scripts/docker_postiz_check_upload.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Script: check_uploads.sh
-# Description: Runs inside the postiz container to check /uploads folder size & file count
-
 CONTAINER_NAME="postiz"
 
 docker exec "$CONTAINER_NAME" bash -c '
@@ -10,7 +7,7 @@ docker exec "$CONTAINER_NAME" bash -c '
     size_h=$(du -sh "$dir" 2>/dev/null | cut -f1)
     size_b=$(du -sb "$dir" 2>/dev/null | cut -f1)
     count=$(find "$dir" -type f | wc -l)
-    # Use tab as separator to avoid spaces confusion
-    printf "%s\t%s\t%d\t%d\n" "$dir" "$size_h" "$size_b" "$count"
-  done | sort -t $'\t' -k3,3n | awk -F $'\t' '"'"'{printf "%-40s | %-8s | %d file(s)\n", $1, $2, $4}'"'"'
+    # use pipe | as separator without spaces
+    printf "%s|%s|%d|%d\n" "$dir" "$size_h" "$size_b" "$count"
+  done | sort -t"|" -k3,3n | awk -F"|" '\''{printf "%-40s | %-8s | %d file(s)\n", $1, $2, $4}'\''
 '
